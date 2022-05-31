620b2b8cfa276bc0fb0afe3711c38df09b7273bb
Lorderite <lorderite@gmail.com>
2022-05-31 22:41:42 +0800

    Fixed formatting?

--- Music Player Project/Account.cs
+++ Music Player Project/Account.cs
@@ -1,55 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Music_Player_Project
 {
 
-    [Serializable]
-    public class Account
-    {
+	[Serializable]
+	public class Account
+	{
 
-        //Variables
-        public string UserName { get; private set; }
-        public string Passkey { get; private set; }
-        public string Salt { get; private set; }
+		//Variables
+		public string UserName { get; private set; }
+		public string Passkey { get; private set; }
+		public string Salt { get; private set; }
 
 
-        /// <summary>
-        /// Constructor for making new account
-        /// </summary>
-        /// <param name="userName"></param>
-        /// <param name="password"></param>
-        public Account(string userName, string password)
-        {
-            //================== Prepare ==================//
+		/// <summary>
+		/// Constructor for making new account
+		/// </summary>
+		/// <param name="userName"></param>
+		/// <param name="password"></param>
+		public Account(string userName, string password)
+		{
+			//================== Prepare ==================//
 
-            //Set username
-            this.UserName = userName;
+			//Set username
+			this.UserName = userName;
 
-            //prepare byte array
-            byte[] saltBytes = new byte[128];
+			//prepare byte array
+			byte[] saltBytes = new byte[128];
 
-            //================== Salting ==================//
+			//================== Salting ==================//
 
-            //Salt password
-            var salter = new RNGCryptoServiceProvider();
-            //Generate salt
-            salter.GetNonZeroBytes(saltBytes);
-            salter.Dispose();
-            //Save Salt
-            this.Salt = Convert.ToBase64String(saltBytes);
+			//Salt password
+			var salter = new RNGCryptoServiceProvider();
+			//Generate salt
+			salter.GetNonZeroBytes(saltBytes);
+			salter.Dispose();
+			//Save Salt
+			this.Salt = Convert.ToBase64String(saltBytes);
 
-            //================== Hashing ==================//
+			//================== Hashing ==================//
 
-            //Prepare hasher
-            var hasher = new Rfc2898DeriveBytes(password, saltBytes, 100);
-            //Get Hash
-            this.Passkey = Convert.ToBase64String(hasher.GetBytes(256));
-            hasher.Dispose();
-        }
-    }
+			//Prepare hasher
+			var hasher = new Rfc2898DeriveBytes(password, saltBytes, 100);
+			//Get Hash
+			this.Passkey = Convert.ToBase64String(hasher.GetBytes(256));
+			hasher.Dispose();
+		}
+	}
 }
--- Music Player Project/Song.cs
+++ Music Player Project/Song.cs
@@ -1,134 +1,130 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media.Imaging;
 
 namespace Music_Player_Project
 {
-    public class Song : IComparable
-    {
-        //Variables
-        protected readonly string songName;
-        private readonly string songPath;
-        private readonly int songIdx;
-        private readonly BitmapImage imageData;
+	public class Song : IComparable
+	{
+		//Variables
+		protected readonly string songName;
+		private readonly string songPath;
+		private readonly int songIdx;
+		private readonly BitmapImage imageData;
 
-        //Gets
-        public string SongName { get => songName; }
-        public string SongPath { get => songPath; }
-        public BitmapImage ImageData { get => imageData; }
-        public int SongIdx { get => songIdx; }
+		//Gets
+		public string SongName { get => songName; }
+		public string SongPath { get => songPath; }
+		public BitmapImage ImageData { get => imageData; }
+		public int SongIdx { get => songIdx; }
 
-        //Constructer
-        public Song(string songName, string songPath, int songIdx)
-        {
-            this.songName = songName;
-            this.songPath = songPath;
-            this.songIdx = songIdx;
-            imageData = GetCoverImage(songPath);
-        }
+		//Constructer
+		public Song(string songName, string songPath, int songIdx)
+		{
+			this.songName = songName;
+			this.songPath = songPath;
+			this.songIdx = songIdx;
+			imageData = GetCoverImage(songPath);
+		}
 
-        //Comparer
-        public int CompareTo(Object obj)
-        {
-            var otherSong = (Song)obj;
+		//Comparer
+		public int CompareTo(Object obj)
+		{
+			var otherSong = (Song)obj;
 
-            if (otherSong == null) return 1;//Handle invalid object 
+			if (otherSong == null) return 1;//Handle invalid object 
 
-            if(otherSong.songName != null)
-            {
-                return songName.CompareTo(otherSong.songName);
-            }
-            else
-            {
-                throw new ArgumentException("Error in comparison, object must be invalid");
-            }
-        }
+			if (otherSong.songName != null)
+			{
+				return songName.CompareTo(otherSong.songName);
+			}
+			else
+			{
+				throw new ArgumentException("Error in comparison, object must be invalid");
+			}
+		}
 
-        //Get File's cover image
-        public BitmapImage GetCoverImage(string songPath)
-        {
-            BitmapImage coverImage;
-            TagLib.File file = TagLib.File.Create(songPath);
-            var firstPic = file.Tag.Pictures.FirstOrDefault();
-            if (firstPic != null)
-            {
-                byte[] pictureData = firstPic.Data.Data;
-                coverImage = ToImage(pictureData);
-            }
-            else
-            {
-                //Set default cover Image
-                coverImage = new BitmapImage(new Uri(@"uiresources\default.png", UriKind.Relative));
-            }
+		//Get File's cover image
+		public BitmapImage GetCoverImage(string songPath)
+		{
+			BitmapImage coverImage;
+			TagLib.File file = TagLib.File.Create(songPath);
+			var firstPic = file.Tag.Pictures.FirstOrDefault();
+			if (firstPic != null)
+			{
+				byte[] pictureData = firstPic.Data.Data;
+				coverImage = ToImage(pictureData);
+			}
+			else
+			{
+				//Set default cover Image
+				coverImage = new BitmapImage(new Uri(@"uiresources\default.png", UriKind.Relative));
+			}
 
-            file.Dispose();
-            return coverImage;
-        }
+			file.Dispose();
+			return coverImage;
+		}
 
-        public void SetFileCoverImage(BitmapImage image)
-        {
-            try
-            {
-                using (TagLib.File file = TagLib.File.Create(SongPath))
-                {
-                    TagLib.Id3v2.AttachedPictureFrame cover = new TagLib.Id3v2.AttachedPictureFrame
-                    {
-                        Type = TagLib.PictureType.FrontCover,
-                        Description = "Cover",
-                        MimeType = System.Net.Mime.MediaTypeNames.Image.Jpeg,
-                        Data = ToByteArray(image),
-                        TextEncoding = TagLib.StringType.UTF16
+		public void SetFileCoverImage(BitmapImage image)
+		{
+			try
+			{
+				using (TagLib.File file = TagLib.File.Create(SongPath))
+				{
+					TagLib.Id3v2.AttachedPictureFrame cover = new TagLib.Id3v2.AttachedPictureFrame
+					{
+						Type = TagLib.PictureType.FrontCover,
+						Description = "Cover",
+						MimeType = System.Net.Mime.MediaTypeNames.Image.Jpeg,
+						Data = ToByteArray(image),
+						TextEncoding = TagLib.StringType.UTF16
 
 
-                    };
-                    file.Tag.Pictures = new TagLib.IPicture[] { cover };
-                    file.Save();
-                }
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Cannot edit song that is Playing","Error");
-            }
-        }
+					};
+					file.Tag.Pictures = new TagLib.IPicture[] { cover };
+					file.Save();
+				}
+			}
+			catch (Exception)
+			{
+				MessageBox.Show("Cannot edit song that is Playing", "Error");
+			}
+		}
 
-        //========================================== Conversion Methods =====================================================//
+		//========================================== Conversion Methods =====================================================//
 
-        public BitmapImage ToImage(byte[] array)
-        {
-            using (var ms = new MemoryStream(array))
-            {
-                var image = new BitmapImage();
-                image.BeginInit();
-                image.CacheOption = BitmapCacheOption.OnLoad;
-                image.StreamSource = ms;
-                image.EndInit();
-                return image;
-            }
-        }
+		public BitmapImage ToImage(byte[] array)
+		{
+			using (var ms = new MemoryStream(array))
+			{
+				var image = new BitmapImage();
+				image.BeginInit();
+				image.CacheOption = BitmapCacheOption.OnLoad;
+				image.StreamSource = ms;
+				image.EndInit();
+				return image;
+			}
+		}
 
-        public byte[] ToByteArray(BitmapImage image)
-        {
-            byte[] array;
-            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
-            encoder.Frames.Add(BitmapFrame.Create(image));
-            using (MemoryStream memStream = new MemoryStream())
-            {
-                encoder.Save(memStream);
-                array = memStream.ToArray();
-            }
-            return array;
-        }
+		public byte[] ToByteArray(BitmapImage image)
+		{
+			byte[] array;
+			JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+			encoder.Frames.Add(BitmapFrame.Create(image));
+			using (MemoryStream memStream = new MemoryStream())
+			{
+				encoder.Save(memStream);
+				array = memStream.ToArray();
+			}
+			return array;
+		}
 
-        public override string ToString()
-        {
-            string output = SongName;
-            return output;
-        }
-    }
+		public override string ToString()
+		{
+			string output = SongName;
+			return output;
+		}
+	}
 }
